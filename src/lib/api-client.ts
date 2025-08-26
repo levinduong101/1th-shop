f84098b92920ae58e4d45f8a434c38d6056a6c6e
landofcoder <landofcoder@gmail.com>
2025-08-26 12:50:19 +0700

    updated api client

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,11 +1,14 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
+const ACCESS_TOKEN = process.env.ACCESS_UPLOAD_TOKEN || process.env.ACCESS_TOKEN ||  '';
+
 const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://merch-base-dev.prowerb.digital',
   timeout: 60000,
   headers: {
     'Content-Type': 'application/json',
+    'Authorization': `Bearer ${ACCESS_TOKEN}`
   },
 });
 
