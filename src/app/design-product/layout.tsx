a950a80a924f16831f95d03a9229bc482bd3c152
Tuan Nam <teddy.nam152@gmail.com>
2025-08-28 15:24:37 +0700

    test2

--- src/app/design-product/layout.tsx
+++ src/app/design-product/layout.tsx
@@ -1,10 +1,10 @@
-import AuthLayout from '@/src/components/layout/AuthLayout';
+// import AuthLayout from '@/src/components/layout/AuthLayout';
 import GridLayout from '@/src/components/layout/GridLayout';
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <AuthLayout>
+    // <AuthLayout>
       <GridLayout>{children}</GridLayout>
-    </AuthLayout>
+    // </AuthLayout>
   );
 }
--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -2,9 +2,22 @@
 
 import { useAuthStore } from '@/src/store/authStore';
 import { redirect } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   const pinCode = useAuthStore((state) => state.pinCode);
+  const [hydrated, setHydrated] = useState(false);
+
+  useEffect(() => {
+    useAuthStore.persist.onFinishHydration(() => {
+      setHydrated(true);
+    });
+  }, []);
+
+  if (!hydrated) {
+    return null;
+  }
+
   if (!pinCode) {
     console.log('No pin code found, redirecting to /landing', pinCode);
     redirect('/landing');
